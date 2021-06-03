5ebfd6164cd67a7053be150b328a14cba00c141e
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-06-03 23:29:22 +0900

    #3.5 Middlewares part One

--- src/server.js
+++ src/server.js
@@ -4,15 +4,15 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-    return res.send("I still love you.");
-};
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
-};
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next();
+  };
+  const handleHome = (req, res) => {
+    return res.send("I love middlewares");
+  };
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+  app.get("/", gossipMiddleware, handleHome);
 
 const handleListening = () => console.log(`Server Listening on http://localhost:${PORT} 🚀`);
 
