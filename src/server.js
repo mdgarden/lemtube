1c6edf441f55eebf6043ed3377954138e6887c45
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-06-06 03:06:35 +0900

    #4.1 Making Our Routers

--- src/server.js
+++ src/server.js
@@ -5,20 +5,36 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
+app.use(logger);
 
-const home = (req, res) => {
-  return res.send("hello");
-};
+const globalRouter = express.Router();
 
-const login = (req, res)  => {
-  return res.send("login");
-};
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo)
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+
+
 
  // 나는 Middleware를 쓸 것이다 & 미들웨어 함수에 있는 내용을 먼저 읽어라
  // Middlewares는 컨트롤러의 역할
-app.use(logger);
-app.get("/", home);
-app.get("/login", login)
+
 
 const handleListening = () => console.log(`Server Listening on http://localhost:${PORT} 🚀`);
 
