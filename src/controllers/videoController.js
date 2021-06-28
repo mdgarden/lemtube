2de94ec999624bbae99d7058422b4fca2ec38e8e
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-06-28 22:49:35 +0900

    #6.12 Our First Query part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,17 +1,24 @@
 import Video from "../models/Video";
 
 export const home = (req, res) => {
-  Video.find({}, (error, videos) => {});
-  return res.render("home", { pageTitle: "Home" });
+  console.log("Start");
+  Video.find({}, (error, videos) => {
+    console.log("Finished");
+    return res.render("home", { pageTitle: "Home", videos });
+  });
+  console.log("I finish first");
 };
+
 export const watch = (req, res) => {
   const { id } = req.params;
   return res.render("watch", { pageTitle: `Watching` });
 };
+
 export const getEdit = (req, res) => {
   const { id } = req.params;
   return res.render("edit", { pageTitle: `Editing ` });
 };
+
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
