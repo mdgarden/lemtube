7d4a4971b115925533a23d5abb3bbcc552916753
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-07-02 22:17:41 +0900

    #7.4 Status Code

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/usercontroller";
+import { edit, remove, logout, see } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout)
+userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/removie", remove);
 userRouter.get(":id", see);
