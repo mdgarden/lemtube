58d4ad8fa3eaa7f2f74c5e92d0db6bcfc43cd755
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-07-06 00:04:43 +0900

    15.1 Sending flash message

--- src/middlewares.js
+++ src/middlewares.js
@@ -20,7 +20,7 @@ export const publicOnlyMiddleware = (req, res, next) => {
   if (!req.session.loggedIn) {
     return next();
   } else {
-    req.flash("error", "Not authorized");
+    req.flash("error", "Log in first.");
     return res.redirect("/");
   }
 };
